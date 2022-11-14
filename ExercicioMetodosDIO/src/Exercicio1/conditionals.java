d2c85e786a06c6d412ca9f5226a655246a0e7cba
Luciana Bastos <lucyy.bastos@gmail.com>
2022-11-14 17:04:37 -0300

    Second commit - from DIO to Eclipse, from Eclipse, to git.

---
+++ ExercicioMetodosDIO/src/Exercicio1/conditionals.java
@@ -0,0 +1,14 @@
+package Exercicio1;
+
+public class conditionals {
+
+	  public static void main(String [] args) {
+	        int grade = 7;
+
+
+	        String result = grade >=7 ? "Approved" : "Failed";
+
+	        System.out.println ("Your grade was: "+grade + ". You were "+ result);
+	    }
+	}
+
---
+++ ExercicioMetodosDIO/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author luciana
+ *
+ */
+module ExercicioMetodosDIO {
+}
